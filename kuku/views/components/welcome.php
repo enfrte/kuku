22d403adb88202516368fb2e23a48866680f6ae7
enfrte <leon.forte.fi@gmail.com>
2023-09-30 09:38:55 +0300

    Added latest commit message

--- kuku/views/components/welcome.php
+++ kuku/views/components/welcome.php
@@ -1,5 +1,8 @@
 <check if="{{ !empty($_SESSION['user']['admin']) }}">
 	<true>
+		<div class="alert alert-info" role="alert">
+			Latest update: {{ @UPDATE_COMMIT_MSG }}
+		</div>
 		<h2 class="pt-5 pb-3">Welcome admin</h2>
 		<p>Use the menu to view courses to edit, or create a new course.</p>
 		<p>When you logout, you can try out the courses as a student.</p>
